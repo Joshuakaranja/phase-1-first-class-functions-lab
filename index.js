1bf9e2b49dcbc5b2a9c382c067f6a29ffc659b28
Joshua Karanja <jokaranja26@gmail.com>
2025-06-28 14:31:17 +0300

    fix tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers){
-    return drivers.slice(0,0.2);
+    return drivers.slice(0,2);
 };
 const returnLastTwoDrivers = function (drivers){
     return drivers.slice(-2);
@@ -12,7 +12,7 @@ function createFareMultiplier (multiplier){
         return fare * multiplier;
     };
 }
-const fareDouble = createFareMultiplier(2);
+const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(drivers , driversSelectorFunction){
